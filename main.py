e7ecda8c370324fd55f8bf28ca246764613993ce
Frank <frank.backs@student.uva.nl>
2017-06-26 21:30:09 +0200

    Animation is now 3d added more keyword agruments

--- main.py
+++ main.py
@@ -8,26 +8,54 @@ from initial_conditions import *
 from make_animation import *
 
 from amuse.community.hermite0.interface import Hermite
+from amuse.community.huayno.interface import Huayno
+
+from time import time as clock_time
 
 
 # No separate evolve file (for now)
 # from evolve.py import *
 
 def main_function(number_of_stars=1000, end_time=10.0 | nbody_system.time,
-                  steps=1000, number_of_workers=5, animate=True, save_animation=True):
-    particles = new_cluster(number_of_stars=number_of_stars)
+                  steps=1000, number_of_workers=5, animate=True, save_animation=True,
+                  Q=0.5, D=2.6, filename="default.mp4", use_huayno=False):
+    """
+    Simulates a cluster of stars with varying masses. 
+    Input: 
+    :param number_of_stars: Number of stars to simulate, default: 1000
+    :param end_time:        Total time at which to stop in nbody_system.time units (I think), 
+                            default 10 | nbody_system.time
+    :param steps:           Total number of steps to save, default: 1000
+    :param number_of_workers: Number of cores/threads to use, does nothing when using Huayno, default: 5
+    :param animate:         Makes an animation of the stars at each step (size depends on star mass), default: True
+    :param save_animation:  Saves the animation as mp4 file, default: True
+    :param Q:               Kinectic to potential energy fraction (I think) default: 0.5
+    :param D:               Measure of fragmentedness, must be between 1.5 and 3, default 2.6
+    :param filename:        Determines the name of the mp4 file, default: "default.mp4"
+    :param use_huayno:      Use alternative Huayno gravity evolver, default: False
+    :return: (array of position arrays at each time step, array of time at each time step) 
+    """
+
+    start_time = clock_time()
+
+    particles = new_cluster(number_of_stars=number_of_stars, Q=Q, D=D)
     particles.scale_to_standard()
 
     # Initialize gravity
-    gravity = Hermite(number_of_workers=number_of_workers)
+    if use_huayno:
+        gravity = Huayno(number_of_stars=number_of_stars)
+    else:
+        gravity = Hermite(number_of_workers=number_of_workers)
+
     gravity.parameters.epsilon_squared = 0.05 | nbody_system.length ** 2
     # gravity.parameters.epsilon_squared = 0.15 | nbody_system.length ** 2 # Used to be this
     gravity.particles.add_particles(particles)
     from_gravity_to_model = gravity.particles.new_channel_to(particles)
 
+    # Initial time (note * end_time for correct units)
     time = 0.0 * end_time
 
-    # Save initial conditions
+    # Save initial conditions and make arrays (lists) for each step
     total_energy_at_t0 = gravity.kinetic_energy + gravity.potential_energy
     save_positions = [particles.position]
     save_masses = [particles.mass]
@@ -50,11 +78,18 @@ def main_function(number_of_stars=1000, end_time=10.0 | nbody_system.time,
 
         print "Time: %.4g" % time.number
 
+    gravity.stop()
+
+    print "It took %.3g seconds clock time" % (clock_time() - start_time)
+
     if animate:
-        make_animation(save_positions, save_masses, times, save_animation=save_animation)
+        make_animation(save_positions, save_masses, times, save_animation=save_animation, filename=filename)
 
-    return save_particles, times
+    return save_positions, times
 
-main_function()
+# callable form command line (only with default settings)
+# to run with different settings import it into a python environment (from main import *)
+if __name__ == "__main__":
+    main_function()
 
 
--- make_animation.py
+++ make_animation.py
@@ -1,3 +1,5 @@
+# Import the whole lot for now, some are not required, but maybe later they will be required.
+
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,28 +7,30 @@ import numpy as np
 from amuse.units import units
 from amuse.units import constants
 from amuse.units import nbody_system
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def update_animation(i, ax, save_positions):
-    ax.set_offsets(zip(save_positions[i][:,0].number, save_positions[i][:,1].number))
-    # print save_particles[i].position[:,0].number
-    return ax
+def update_animation(i, graph, save_positions):
+    graph._offsets3d = (save_positions[i][:, 0].number, save_positions[i][:, 1].number, save_positions[0][:, 2].number)
+    return graph
 
 
-def make_animation(save_positions, save_masses, times, save_animation=False):
+def make_animation(save_positions, save_masses, times, save_animation=False, filename="default.mp4"):
     """
-    Makes an animation!
+    Makes an animation! Yay! Returns nothing. For more information see main.py. 
     """
     f = plt.figure()
-    ax = plt.scatter(save_positions[0][:,0].number, save_positions[0][:,1].number,
-                     color="red", s=np.array(save_masses[0].number) * 1000)
-    plt.xlim([-5, 5])
-    plt.ylim([-5, 5])
+    ax = plt.subplot(111, projection="3d")
+    graph = ax.scatter(save_positions[0][:, 0].number, save_positions[0][:, 1].number, save_positions[0][:, 2].number,
+                       color="red", s=np.array(save_masses[0].number) * 1000)
+    ax.set_xlim([-2.5, 2.5])
+    ax.set_ylim([-2.5, 2.5])
+    ax.set_zlim([-2.5, 2.5])
 
-    ani = animation.FuncAnimation(f, update_animation, frames=len(times), fargs=(ax, save_positions), interval=20)
+    ani = animation.FuncAnimation(f, update_animation, frames=len(times), fargs=(graph, save_positions), interval=20)
 
     if save_animation:
-        ani.save("Fractal_clusters.mp4", writer="ffmpeg", bitrate=-1, codec="h264")
-        
+        ani.save(filename, writer="ffmpeg", bitrate=-1, codec="h264")
+
     plt.show()
 
